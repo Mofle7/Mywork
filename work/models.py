8063e6c729f0364f16b08c48b69ff5b4b4455bd8
Mofle7 <mofleh1416@gmail.com>
2016-10-30 12:58:38 +0300

    Hello

--- work/models.py
+++ work/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Profile(models.Model):
     user = models.OneToOneField (User)
     name = models.CharField(max_length=40)
