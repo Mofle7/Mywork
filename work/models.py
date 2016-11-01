68538be2ab2c2c9e29d274b9b7f6022ea00161f1
Mofle7 <mofleh1416@gmail.com>
2016-11-01 23:23:12 +0300

    Tue at night

--- work/models.py
+++ work/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8  -*-
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
@@ -5,21 +6,21 @@ from django.forms import ModelForm
 
 
 class Profile(models.Model):
-    user = models.OneToOneField (User)
-    name = models.CharField(max_length=40)
-    mobile = models.CharField(max_length = 10)
+    user = models.OneToOneField (User, verbose_name='User Profile')
+    name = models.CharField('User Name',max_length=40)
+    mobile = models.CharField('user Mobile',max_length = 10)
 
 class Worker(models.Model):
-    user = models.OneToOneField (User)
-    image = models.FileField(upload_to='profile_image')
-    worker_services = models.TextField()
-    worker_experince = models.TextField()
+    user = models.OneToOneField (User, verbose_name= 'Worker User')
+    image = models.FileField(upload_to='profile image')
+    worker_services = models.TextField('Worker Service')
+    worker_experince = models.TextField('Worker Experience')
     creat_date = models.DateTimeField(auto_now_add=True)
 
 
 class Adv(models.Model):
-    user = models.ForeignKey(User)
-    adv_text = models.TextField()
+    user = models.ForeignKey(User, verbose_name='Advertise')
+    adv_text = models.TextField('Write Your Advertise')
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
--- work/views.py
+++ work/views.py
@@ -2,44 +2,48 @@ from django.shortcuts import render, get_object_or_404
 from django.forms import modelformset_factory
 from work.models import Adv, Profile, Worker
 from django.forms import ModelForm
+from django.http import HttpResponseRedirect, reverse
 
+def main(requset):
+    advs = Adv.objects.all()
+    workers = Worker.objects.all()
+    return render(requset,'work/main.html',{'advs':advs},{'workers':workers})
 
-def main():
-    pass
-def home():
-    pass
 def support():
     pass
+
 def about_us():
     pass
 
 #############################
 
 def show_adv(requset, pk):
-    adv = get_object_or_404(requset, pk)
+    adv = get_object_or_404(Adv, pk=pk)
     return render (requset,'work/show_adv.html',{'adv':adv})
 
-def add_adv(requset, pk):
+def add_adv(requset):
     if requset.method == 'POST':
-        instance = Adv (user = 'user')
+        instance = Adv (user = requset.user)
         form = AdvForm(requset.POST, instance = instance)
         if form.is_valid :
             form.save()
+            return HttpResponseRedirect(reverse('show_adv'))
     else:
-        form = AdvForm
+        form = AdvForm()
     return render (requset,'work/add_adv.html',{"form":form})
-    return HttpResponseRedirect(reverse('show_adv'))
+
 
 def edit_adv(requset, pk):
-    if requset.method == 'GET':
-        added_adv = Adv (user='user', adv_text='adv_text')
-        form = AdvForm (requset.POST, instance=added_adv)
+    adv = get_object_or_404(Adv, pk=pk)
+    if requset.method == 'POST':
+        form = AdvForm (requset.POST, instance=adv)
         if form.is_valid:
             form.save()
+            return HttpResponseRedirect(reverse('edit_adv'))
     else :
-        form = AdvForm
+        form = AdvForm(instance=adv)
     return render (requset,'edit_adv.html',{"form":form})
-    return HttpResponseRedirect(reverse('edit_adv'))
+
 
 def del_adv():
     pass
@@ -52,25 +56,27 @@ def show_profile(requset, pk):
 
 def add_profile(requset,pk):
     if requset.method == 'POST':
-        instance = Profile (user = 'user')
+        instance = Profile (user = requset.user)
         form = ProfileForm(requset.POST, instance = instance)
         if form.is_valid :
             form.save()
+            return HttpResponseRedirect(reverse('show_profile'))
     else:
-        form = ProfileForm
+        form = ProfileForm()
     return render (requset,'add_profile.html',{"form":form})
-    return HttpResponseRedirect(reverse('show_profile'))
+
 
 def edit_profile(requset, pk):
-    if requset.method == 'GET':
-        added_profile = Profile (user='user',name='name', mobile='mobile')
-        form = ProfileForm (requset.POST, instance=added_profile)
+    profile = get_object_or_404(Profile, pk=pk)
+    if requset.method == 'POST':
+        form = ProfileForm (requset.POST, instance=profile)
         if form.is_valid:
             form.save()
+            return HttpResponseRedirect(reverse('show_profile'))
     else :
-        form = ProfileForm
+        form = ProfileForm(instance=profile)
     return render (requset,'edit_profile.html',{"form":form})
-    return HttpResponseRedirect(reverse('show_profile'))
+
 def del_profile():
     pass
 
@@ -84,19 +90,19 @@ def forget_password():
 ### Models Form ###
 
 class AdvForm(ModelForm):
-    class Meat:
+    class Meta:
         model = Adv
         fields = ['adv_text']
 
 class ProfileForm(ModelForm):
-    class Meat:
+    class Meta:
         model = Profile
         fields = ['name','mobile']
 
 class WorkerForm(ModelForm):
-    class Meat:
+    class Meta:
         model = Worker
-        fields = ['image','worker_services', 'worker_experiences']
+        fields = ['image','worker_services', 'worker_experince']
 
 # Create your views here.
 # Create your views here.
